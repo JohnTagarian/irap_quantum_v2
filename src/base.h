a3d7b94779aff7020b707d079e9efbc5a4ef4726
Kantinan <pondkitnag@gmail.com>
2023-11-16 19:19:39 +0700

    1st path Non analomaly detect

--- src/base.h
+++ src/base.h
@@ -190,14 +190,30 @@ class Wheels{
 
 class Control : public Wheels , public IMU_t{
 
-    float imu_error , imu_error_sum,imu_error_diff,imu_prev_error,imu_control=0.0f;
+    
     unsigned long imu_current_time,time,imu_last_time,imu_dt_time;
 
     float kp_imu = 16.0;
     float ki_imu = 0.001; // 35.5
     float kd_imu = 0.0;
 
+    const float consy = 3184.2f;
+    const float consx = 3193.8f;
+    
+    float p2p_kp = 150.0 ,p2p_ki = 0.001 ,p2p_kd = 0.05;
+    // float p2p_kp = 100.0 ,p2p_ki = 0.0 ,p2p_kd = 0.00;
+    
+    unsigned long dt;
     public:
+        float imu_error , imu_error_sum,imu_error_diff,imu_prev_error,imu_control=0.0f;
+        float* distance;
+        uint8_t p2p_cnt;
+        float p2p_sum_error ,p2p_prev_disp;
+        float pvector[2] = {0.0f,0.0f};
+        float dvector[2];
+        float px,py;
+        float error_heading;
+
         float control_heading(int target_angle ,int alp , float vr){
             imu_current_time = micros();
             call_bno();      
@@ -219,7 +235,7 @@ class Control : public Wheels , public IMU_t{
 
             imu_control = constrain(imu_control, -500, 500);
             move(vr,alp,imu_control*-1);
-            Serial.printf("yaw = %f error= %f \t control= %f  sum_error= %f termI= %f\n",yaw,imu_error,imu_control,imu_error_sum,ki_imu*imu_error_sum);
+            // Serial.printf("yaw = %f error= %f \t control= %f  sum_error= %f termI= %f\n",yaw,imu_error,imu_control,imu_error_sum,ki_imu*imu_error_sum);
 
             imu_prev_error = imu_error;
 
@@ -229,4 +245,103 @@ class Control : public Wheels , public IMU_t{
             
             return imu_error;
         }
+
+        float* get_distance_vector(void){
+
+            call_bno();
+            // Serial.printf("%f\n",yawValue);
+            // dvector[0] = (kENC1*(enc_cnt[0])*sin(120*DEG_TO_RAD) + kENC2*(enc_cnt[1])*sin(240*DEG_TO_RAD)) * 0.666666667; //y 
+            // dvector[1] = (kENC1*(enc_cnt[0])*cos(120*DEG_TO_RAD) + kENC2 * (enc_cnt[1])*cos(240*DEG_TO_RAD) * kENC2 + enc_cnt[2] *kENC3)*0.78125; // x
+            // Serial.printf("%d , %d , %d , %f , %f\n",enc_cnt[0],enc_cnt[1],enc_cnt[2],pvector[0],pvector[1]);// Serial.println(pvector[0]);
+    
+            float raw_y = (enc_cnt[1]*sin(300*DEG_TO_RAD)) + (enc_cnt[2]*sin(60*DEG_TO_RAD));
+            float raw_x = (enc_cnt[0]*cos(180*DEG_TO_RAD)) + (enc_cnt[1]*cos(300*DEG_TO_RAD)) +(enc_cnt[2]*cos(60*DEG_TO_RAD));
+            
+            float y = raw_y/consy;
+            float x = raw_x/consx;
+            
+            dvector[0] = pvector[0] + (x-px)*sin((360 - yaw)*DEG_TO_RAD) +  (y-py)*cos((360 - yaw)*DEG_TO_RAD); //y 
+            dvector[1] = pvector[1] + (x-px)*cos((360 - yaw)*DEG_TO_RAD) - (y-py)*sin((360 - yaw)*DEG_TO_RAD);//x
+
+            // Serial.printf("%d , %d , %d x =%f , y = %f , cx = %f , cy = %f yaw = %f\n",enc_cnt[0],enc_cnt[1],enc_cnt[2],x ,y,dvector[1],dvector[0],yawValue);
+            px = x;
+            py = y;
+            pvector[0] = dvector[0];
+            pvector[1] = dvector[1];
+            // Serial.printf("rawY = %f rawX = %f y = %f x = %f yaw = %f\n",raw_y,raw_x,y,x,yaw);
+
+            // Serial.printf("Y = %f X = %f Transf_Y = %f Transf_x = %f yaw = %f\n",y,x,dvector[0],dvector[1],yaw);
+
+            return dvector;
+            // Serial.printf("%d , %d , %d , %f , %f\n",enc_cnt[0],enc_cnt[1],enc_cnt[2],pvector[0],pvector[1]);// Serial.println(pvector[0]);
+        
+        }
+
+
+        float p2p(float yg ,float xg ,int angle){
+            distance = get_distance_vector();
+            float x = *(distance+1);
+            float y = *distance;
+
+            float disp_error = sqrt((xg-x)*(xg-x) + (yg-y)*(yg-y));
+            float alp = fmod((360 + (atan2(yg-y, xg-x) * RAD_TO_DEG)), 360);
+
+            unsigned long current_time = micros();
+            p2p_sum_error += disp_error;
+
+            float control_displecement  =  p2p_kp*disp_error + p2p_ki*p2p_sum_error + p2p_kd*(disp_error - p2p_prev_disp)/dt;
+            
+            error_heading = control_heading(angle,fmod(alp+yaw,360),control_displecement);
+            if(disp_error < 0.005)p2p_sum_error=0;
+            // Serial.printf("y = %f ,x = %f\n",dvector[0],dvector[1]);
+            p2p_prev_disp = disp_error;
+            dt = current_time;
+
+            Serial.printf("error_yaw = %f error = %f sum_heading = %f control = %f sum_error = %f p2p_cnt = %d\n",error_heading,disp_error,imu_error_sum,control_displecement,p2p_sum_error,p2p_cnt);
+
+            // float detlas = sqrt(pow(pos.x-posx_prev,2)+pow(pos.y-posy_prev,2));
+            // vel = deltas/0.1;
+            // posx_prev = pos.x;
+            // posy_prev = pos.y;
+            return disp_error;
+
+        
+    }
+
+      
+
+};
+
+class Navigation : public Control{
+  
+  public:
+    void via_navigate(float yg , float xg ,int angle , float speed){
+      while(1){
+        distance = get_distance_vector();
+        float x = *(distance+1);
+        float y = *distance;
+        float disp_error = sqrt((xg-x)*(xg-x) + (yg-y)*(yg-y));
+        float alp = fmod((360 + (atan2(yg-y, xg-x) * RAD_TO_DEG)), 360);
+
+        error_heading = control_heading(angle,fmod(alp+yaw,360),speed);
+        if(disp_error < 0.025){
+          break;
+        }
+        Serial.printf("error_yaw = %f error = %f\n",error_heading,disp_error);
+      }
+        
+  }
+  void p2p_navigate(float yg , float xg , int angle){
+    p2p_cnt = 0;
+    p2p_sum_error = 0;
+    imu_error_sum =0;
+
+    
+    while(1){
+      if(p2p(yg,xg,angle) < 0.05){
+        if(++p2p_cnt >= 200)break;
+      }
+    }
+  }
+
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,43 @@
 #include <base.h>
 
 IMU_t IMU;
+Wheels wheels;
 Control control;
+Navigation navigation;
 
 void setup() {
   Serial.begin(115200);
   initial_motor();
   IMU.initial();
+  delay(1000);
 
   
 }
+float prev_pitch , prev_roll;
+float timer_obs;
 
 void loop() {
-  control.control_heading(0,90,100);
-  // control.move(0,0,800);
+  // navigation.via_navigate(-0.5,0.0,0,50.0);
+  // wheels.stop();
+  // delay(500);
+  // navigation.via_navigate(-0.5,0.51,0,50.0);
+  // wheels.stop();
+  // delay(500);
+  // // wheels.move(120,90,0);
+  while(1){
+    IMU.call_bno();
+    Serial.printf("diff pitch : %f diff roll :%f\n ",IMU.pitch - prev_pitch,IMU.roll - prev_roll);
+    prev_pitch = IMU.pitch;
+    control.control_heading(0,90,100);
   
-  // Serial.printf("Yaw : %f\n",IMU.yaw);
+  }
+  // control.p2p(0.0,1.0,0);
+  // control.control_heading(0,90,100);
+  // control.get_distance_vector();
+  
+  // Serial.printf("Pitch :%f , Roll :%f\n",IMU.pitch,IMU.roll);
   // wheels.move(100,120,0);
-  // wheels.cmd_motor(0,0,50);
+  // control.cmd_motor(-50,-50,-50);
   // Serial.printf("ENC1: %d , ENC2: %d , ENC3: %d\n",enc_cnt[0],enc_cnt[1],enc_cnt[2]);
 
 }
